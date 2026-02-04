6f6dd7543ecbc835f3c473cc742ac85dce461ee8
RajanBakshi238 <rajan.bakshi238@gmail.com>
2026-02-05 03:28:59 +0530

    Home screen intialized

---
+++ app/index.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+export default function HomeScreen() {
+    return (
+        <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 16 }}>Home Screenddd</Text>
+    );
+}
