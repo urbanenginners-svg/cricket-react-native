cab7d133b9f283612f28e485d8c352815808d7d4
RajanBakshi238 <rajan.bakshi238@gmail.com>
2026-02-10 02:24:37 +0530

    header hided from first splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,5 +4,5 @@ import "../global.css"; // 👈 MUST be first import
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-    return <Stack />;
+    return <Stack screenOptions={{ headerShown: false }} />;
 }
