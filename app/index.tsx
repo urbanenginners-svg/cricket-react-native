7a0684d01e0602a5f59ae459f42370995c21b6dc
RajanBakshi238 <rajan.bakshi238@gmail.com>
2026-02-10 01:15:02 +0530

    logo added , and there metro config file added with svg declare types

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,12 @@
 import { Text, View } from "react-native";
 
+import CricketLogo from "../assets/svg/cricket_logo.svg";
+
+
 export default function HomeScreen() {
     return (
         <View className="">
+            <CricketLogo width={140} height={140} />
             <Text>Home</Text>
         </View>
     );
