c8bc6d4cec1877dfc88f4de37180451a764148d2
RajanBakshi238 <rajan.bakshi238@gmail.com>
2026-02-10 02:21:17 +0530

    starting  splash screen created

--- app/index.tsx
+++ app/index.tsx
@@ -5,9 +5,8 @@ import CricketLogo from "../assets/svg/cricket_logo.svg";
 
 export default function HomeScreen() {
     return (
-        <View className="flex-1 bg-blue-600 justify-center items-center">
-            <Text className="text-red-600 text-2xl">Home</Text>
-            <CricketLogo width={140} height={140} />
+        <View className="flex-1 bg-blue_dark justify-center items-center">
+            <CricketLogo />
         </View>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],presets: [require("nativewind/preset")],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"], presets: [require("nativewind/preset")],
   theme: {
     extend: {
       colors: {
         primary: "#2563EB",
         background: "#F9FAFB",
-        dark: "#111827",
+        blue_dark: "#181823",
       },
     },
   },
